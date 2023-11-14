f3d550cbac0a559f1868291c51af8be66dcdb308
Martín Palermo <moshitin.encoded@gmail.com>
2023-11-14 16:59:54 -0300

    BehaviourTreeMachine Refactor
    Docs updated and inheritance support removed.

--- Runtime/BehaviourTreeMachine.cs
+++ Runtime/BehaviourTreeMachine.cs
@@ -31,10 +31,15 @@ namespace MoshitinEncoded
         /// </summary>
         public event System.Action PreUpdate;
 
-        [SerializeField, Tooltip("The Behaviour Tree that will run on this machine.")]
+        /// <summary>
+        /// Called after the Behaviour Tree update.
+        /// </summary>
+        public event System.Action Updated;
+
+        [SerializeField]
         private BehaviourTree _BehaviourTree;
 
-        [SerializeField, Tooltip("How the Behaviour Tree will be updated.")]
+        [SerializeField, Tooltip("How and when the Behaviour Tree will be updated.")]
         private UpdateModeEnum _UpdateMode = UpdateModeEnum.Update;
 
         private BehaviourTree _BehaviourTreeInstance;
@@ -86,9 +91,11 @@ namespace MoshitinEncoded
                 return;
             }
 
-            OnPreUpdate();
             PreUpdate?.Invoke();
+
             _BehaviourTreeInstance.Update();
+
+            Updated?.Invoke();
         }
 
         /// <summary>
@@ -104,8 +111,8 @@ namespace MoshitinEncoded
         /// <summary>
         /// Returns the value of a parameter in the Behaviour Tree.
         /// </summary>
-        /// <typeparam name="T"> The parameter Type. Inheritance is supported. </typeparam>
-        /// <param name="name"> The parameter name. </param>
+        /// <typeparam name="T"> Parameter Type. (Inheritance supported). </typeparam>
+        /// <param name="name"> Parameter name as shown on the blackboard. </param>
         /// <returns> The parameter value if found, <b>default</b> value otherwise. </returns>
         public T GetParameter<T>(string name) =>
             _BehaviourTreeInstance.GetParameter<T>(name);
@@ -113,27 +120,16 @@ namespace MoshitinEncoded
         /// <summary>
         /// Sets the value of a parameter in the Behaviour Tree.
         /// </summary>
-        /// <typeparam name="T"> The parameter Type. Inheritance is supported. </typeparam>
-        /// <param name="name"> The parameter name. </param>
-        /// <param name="value"> The parameter value to set. </param>
+        /// <typeparam name="T"> Parameter Type. (Inheritance supported). </typeparam>
+        /// <param name="name"> Parameter name as shown on the blackboard. </param>
+        /// <param name="value"> New parameter value. </param>
         public void SetParameter<T>(string name, T value) => 
             _BehaviourTreeInstance.SetParameter(name, value);
-
-        /// <summary>
-        /// Called after <i>Awake</i> and <i>ChangeBehaviourTree</i> methods. Useful to <b>set</b> parameters.
-        /// </summary>
-        protected virtual void OnInitialized() {}
-
-        /// <summary>
-        /// Called before the Behaviour Tree update. Useful to <b>update</b> parameters.
-        /// </summary>
-        protected virtual void OnPreUpdate() {}
 
         private void InitializeBehaviourTree()
         {
             _BehaviourTreeInstance = _BehaviourTree.Clone();
             _BehaviourTreeInstance.Bind(this);
-            OnInitialized();
             Initialized?.Invoke();
         }
     }
