b045b73764b8c47fb30dfd85c744ec564c4e274b
Martín Palermo <moshitin.encoded@gmail.com>
2023-11-29 20:00:59 -0300

    Add LogParameterNode

---
+++ Runtime/Nodes/Action/LogParameterNode.cs
@@ -0,0 +1,31 @@
+using MoshitinEncoded.GraphTools;
+
+using UnityEngine;
+
+namespace MoshitinEncoded.AI.BehaviourTreeLib
+{
+    [CreateNodeMenu(path: "Action/Debug/Log Parameter")]
+    public class LogParameterNode : ActionNode
+    {
+        [Space]
+        [SerializeField] private string _ParameterName;
+
+        private BlackboardParameter _Parameter;
+
+        protected override void OnInitialize(BehaviourTreeRunner runner)
+        {
+            _Parameter = runner.GetParameterByRef(_ParameterName);
+        }
+
+        protected override NodeState OnUpdate(BehaviourTreeRunner runner)
+        {
+            if (_Parameter == null)
+            {
+                return NodeState.Failure;
+            }
+
+            Debug.Log(_ParameterName + " Parameter: " + _Parameter.BoxedValue);
+            return NodeState.Success;
+        }
+    }
+}
